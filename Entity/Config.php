77c1319d5e5fc00d24b61002e4c703a9a1330416
Johanna KELLER <johanna.keller@esial.net>
2011-12-18 14:48:55 +0100

    Add Configuration part of the model.

---
+++ Entity/Config.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace IMRIM\Bundle\LmsBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * IMRIM\Bundle\LmsBundle\Entity\Config
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Config
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name = "id", type = "integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy = "AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string $nameParameter
+     *
+     * @ORM\Column(name = "nameParameter", type = "string", length = 255, nullable = false)
+     * @Assert\NotNull()
+     * @Assert\NotBlank()
+     * @Assert\MaxLength(255)
+     * @Assert\Regex("/^[a-zA-Z0-9._-]+$/")
+     */
+    private $nameParameter;
+
+    /**
+     * @var text $value
+     *
+     * @ORM\Column(name = "value", type = "text", nullable = false)
+     * @Assert\NotNull()
+     */
+    private $value;
+
+    /**
+     * @var datetime $creationTime
+     *
+     * @ORM\Column(name = "creationTime", type = "datetime", nullable = false)
+     * @Assert\NotNull()
+     * @Assert\DateTime()
+     */
+    private $creationTime;
+
+    /**
+     * @var datetime $updateTime
+     *
+     * @ORM\Column(name = "updateTime", type = "datetime", nullable = false)
+     * @Assert\NotNull()
+     * @Assert\DateTime()
+     */
+    private $updateTime;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nameParameter
+     *
+     * @param string $nameParameter
+     */
+    public function setNameParameter($nameParameter)
+    {
+        $this->nameParameter = $nameParameter;
+    }
+
+    /**
+     * Get nameParameter
+     *
+     * @return string 
+     */
+    public function getNameParameter()
+    {
+        return $this->nameParameter;
+    }
+
+    /**
+     * Set value
+     *
+     * @param text $value
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * Get value
+     *
+     * @return text 
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * Set creationTime
+     *
+     * @param datetime $creationTime
+     */
+    public function setCreationTime($creationTime)
+    {
+        $this->creationTime = $creationTime;
+    }
+
+    /**
+     * Get creationTime
+     *
+     * @return datetime 
+     */
+    public function getCreationTime()
+    {
+        return $this->creationTime;
+    }
+
+    /**
+     * Set updateTime
+     *
+     * @param datetime $updateTime
+     */
+    public function setUpdateTime($updateTime)
+    {
+        $this->updateTime = $updateTime;
+    }
+
+    /**
+     * Get updateTime
+     *
+     * @return datetime 
+     */
+    public function getUpdateTime()
+    {
+        return $this->updateTime;
+    }
+}
