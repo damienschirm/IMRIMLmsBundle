912d25e489677933433e1b3f61343841d7f36501
Romain HORY <romain.hory@gmail.com>
2011-12-22 12:35:39 +0100

    Mise en place du css

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -14,6 +14,7 @@ class DefaultController extends Controller
      */
     public function indexAction($name)
     {
-        return array('name' => $name);
+		return $this->render('IMRIMLmsBundle:Default:index.html.twig', array('name' =>$name));
+        //return array('name' => $name);
     }
 }
