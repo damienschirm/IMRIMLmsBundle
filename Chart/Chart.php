6f0a6448150ddabcc42ec1fe0d05596047a4a656
Damien SCHIRM <damien.schirm@esial.net>
2011-12-29 22:37:48 +0100

    Statistics: Number of users enrolled into a course + average of the time spent by user

--- Chart/Chart.php
+++ Chart/Chart.php
@@ -8,53 +8,23 @@ include('Chart' . DIRECTORY_SEPARATOR . "IChart.php");
  */
 class Chart implements IChart {
 
-    function pieChartJS() {
-        $scriptJS = "<script type='text/javascript' src='https://www.google.com/jsapi'></script>"
+    public function numberOfUsers($numberOfEnrolment) {
+        $scriptJS = "<div id='colchart_div'>"
+                . "<script type='text/javascript' src='https://www.google.com/jsapi'></script>"
                 . "<script type='text/javascript'>"
                 . "google.load('visualization', '1', {packages:['corechart']});"
                 . "google.setOnLoadCallback(drawChart);"
                 . " function drawChart() { "
                 . "   var data = new google.visualization.DataTable();"
-                . "   data.addColumn('string', 'Tâches');"
-                . "   data.addColumn('number', 'Heures par jour');"
+                . "   data.addColumn('string', 'Utilisateurs inscrits');"
+                . "   data.addColumn('number', 'Effectif');"
                 . "   data.addRows(["
-                . "     ['Travail',    11],"
-                . "     ['Manger',      2],"
-                . "     ['Trajet',  2],"
-                . "     ['TV', 2],"
-                . "     ['Dormir',    7]"
+                . "     ['Utilisateurs inscrits',    " . $numberOfEnrolment . "],"
                 . "   ]);"
                 . "   var options = {"
-                . "     width: 450, height: 300,"
-                . "     title: 'Mes activités journalières'"
+                . "     width: 400, height: 240,"
                 . "   };"
-                . "   var chart = new google.visualization.PieChart(document.getElementById('chart_div'));"
-                . "   chart.draw(data, options);"
-                . " }"
-                . "</script>";
-
-        return $scriptJS;
-    }
-
-    public function numberOfUsers() {
-        $scriptJS = "<div id='geochart_div'>"
-                . "<script type='text/javascript' src='https://www.google.com/jsapi'></script>"
-                . "<script type='text/javascript'>"
-                . "google.load('visualization', '1', {packages:['geochart']});"
-                . "google.setOnLoadCallback(drawChart);"
-                . " function drawChart() { "
-                . "   var data = new google.visualization.DataTable();"
-                . "   data.addColumn('string', 'Pays');"
-                . "   data.addColumn('number', 'Utilisateurs inscrits');"
-                . "   data.addRows(["
-                . "     ['France',    1],"
-                . "     ['Allemagne',      1]"
-                . "   ]);"
-                . "   var options = {"
-                . "     width: 700, height: 400,"
-                . "     title: 'Géolocalisation des personnes inscrites'"
-                . "   };"
-                . "   var chart = new google.visualization.GeoChart(document.getElementById('geochart_div'));"
+                . "   var chart = new google.visualization.ColumnChart(document.getElementById('colchart_div'));"
                 . "   chart.draw(data, options);"
                 . " }"
                 . "</script>"
@@ -63,29 +33,29 @@ class Chart implements IChart {
         return $scriptJS;
     }
 
-    function usersEnrolledInCourse(Controller $ctrl) {
+    public function usersEnrolledInCourse(Controller $ctrl) {
         $courses = $ctrl->getDoctrine()
                 ->getRepository('IMRIMLmsBundle:Course')
                 ->findAll();
         $i = 0;
+        $temp="";
         foreach ($courses as $course) {
             $i++;
             $qb = $ctrl->getDoctrine()->getEntityManager()->createQueryBuilder()
                     ->select('COUNT(ue.user)') //$i[0][1] 
-                    ->from('IMRIMLmsBundle:UserEnrolment', 'ue');
-                    //->from('IMRIMLmsBundle:Course', 'c')
-                    //->where('c.course_id = ?',$course->getId());//'ue.course = ?',$course->);//:course_id')
-                    //->setParameter(':course_id',$course->getId());
+                    ->from('IMRIMLmsBundle:UserEnrolment', 'ue')
+                    ->where('ue.course = :course')
+                    ->setParameter('course',$course);
             $q = $qb->getQuery();
             $result = $q->getSingleScalarResult();
             
-            $temp = "[" . $course->getName() . "," . $result . "]";
+            $temp = $temp."['" . $course->getName() . "'," . $result . "]";
             if ($i<count($courses)){
                 $temp = $temp . ",";
             }
         }
 
-        $scriptJS = "<div id='geochart_div'>"
+        $scriptJS = "<div id='piechart_div'>"
                 . "<script type='text/javascript' src='https://www.google.com/jsapi'></script>"
                 . "<script type='text/javascript'>"
                 . "google.load('visualization', '1', {packages:['corechart']});"
@@ -96,11 +66,6 @@ class Chart implements IChart {
                 . "   data.addColumn('number', 'Pourcentage');"
                 . "   data.addRows(["
                 . $temp
-                /*. "     ['Travail',    11],"
-                . "     ['Manger',      2],"
-                . "     ['Trajet',  2],"
-                . "     ['TV', 2],"
-                . "     ['Dormir',    7]"*/
                 . "   ]);"
                 . "   var options = {"
                 . "     width: 450, height: 300,"
@@ -113,6 +78,59 @@ class Chart implements IChart {
                 . "</div>";
 
         return $scriptJS;
+    }
+
+    public function averageOfTimeSpentByCourse(Controller $ctrl) {
+        $users = $ctrl->getDoctrine()
+                ->getRepository('IMRIMLmsBundle:User')
+                ->findAll();
+        
+        $temp = "";
+        $i = 0;
+        foreach($users as $u){
+            $i++;
+            $qb = $ctrl->getDoctrine()->getEntityManager()->createQueryBuilder()
+                    ->select('AVG(ue.timeSpent)')  
+                    ->from('IMRIMLmsBundle:UserEnrolment', 'ue')
+                    ->where('ue.user = :user')
+                    ->setParameter('user',$u);
+            $q = $qb->getQuery();
+            $result = $q->getSingleScalarResult();
+            if (!isset($result)){
+                $result = 0;
+            }
+            $temp = $temp."['" . $u->getLogin() . "'," . $result . "]";
+            if ($i<count($users)){
+                $temp = $temp . ",";
+            }
+        }
+        
+        $scriptJS = "<div id='barchart_div'>"
+                . "<script type='text/javascript' src='https://www.google.com/jsapi'></script>"
+                . "<script type='text/javascript'>"
+                . "google.load('visualization', '1', {packages:['corechart']});"
+                . "google.setOnLoadCallback(drawChart);"
+                . " function drawChart() { "
+                . "   var data = new google.visualization.DataTable();"
+                . "   data.addColumn('string', 'Utilisateurs');"
+                . "   data.addColumn('number', 'Temps moyen');"
+                . "   data.addRows(["
+                . $temp
+                . "   ]);"
+                . "   var options = {"
+                . "     width: 400, height: 240,"
+                . "   };"
+                . "   var chart = new google.visualization.BarChart(document.getElementById('barchart_div'));"
+                . "   chart.draw(data, options);"
+                . " }"
+                . "</script>"
+                . "</div>";
+        
+        return $scriptJS;
+    }
+
+    public function listOfUsersNotHavingFinishedCourse(Controller $ctrl) {
+        
     }
 
 }
--- Controller/ChartController.php
+++ Controller/ChartController.php
@@ -23,8 +23,6 @@ class ChartController extends Controller {
     public function ChartAction() {
         $p = new \Chart();
         //-------- Statistic : number of users ------
-        $scriptJSGeo = $p->numberOfUsers();
-        
         $qb = $this->container->get('doctrine')->getEntityManager()->createQueryBuilder()
                 ->select('COUNT(u.login)') //$i[0][1] 
                 //->addselect('u.login') //$i[0]['login']
@@ -33,18 +31,18 @@ class ChartController extends Controller {
         $i = $q->getSingleScalarResult();
         //getResult(); // retourne un array d'objet.
         //$t = $this->container->get('doctrine')->getEntityManager()->find('IMRIMLmsBundle:User', 1);
+        $scriptJSCol = $p->numberOfUsers($i);
         //-----------------------------------------------------
-        //------- Statistic : % of users enrolled -------
-        $courses = $this->getDoctrine()
-                ->getRepository('IMRIMLmsBundle:Course')
-                ->findAll();
-        
+        //------- Statistic : % of users enrolled -------        
         $scriptJSPie = $p->usersEnrolledInCourse($this);
         //-----------------------------------------------------
+        //------- Statistic : average of the time spent by user by course ---- -------
+        $scriptJSBar = $p->averageOfTimeSpentByCourse($this);
+        
         return $this->render('IMRIMLmsBundle:Chart:chart.html.twig', array('nbOfUsers' => $i,
-            'scriptJSGeo' => $scriptJSGeo,
-            'courses' => $courses,
-            'scriptJSPie' => $scriptJSPie));
+            'scriptJSCol' => $scriptJSCol,
+            'scriptJSPie' => $scriptJSPie,
+            'scriptJSBar' => $scriptJSBar));
     }
     
 }
