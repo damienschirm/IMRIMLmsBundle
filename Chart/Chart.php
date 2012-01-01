beaec2f7bb988bc0fa3a2cff43f538eebfb5ffea
Damien SCHIRM <damien.schirm@esial.net>
2012-01-01 19:19:03 +0100

    Statistics: PDF report with DomPdf, problem with the Javascript part

--- Chart/Chart.php
+++ Chart/Chart.php
@@ -238,6 +238,20 @@ class Chart implements IChart {
 
         return $scriptJS;
     }
+    
+        function get_content($url)
+    {
+       $ch = curl_init();
+       curl_setopt ($ch, CURLOPT_URL, $url);
+       curl_setopt ($ch, CURLOPT_HEADER, 0);
+       ob_start();
+       curl_exec ($ch);
+       curl_close ($ch);
+       $string = ob_get_contents();
+       ob_end_clean();
+     
+       return $string;   
+    }
 
 }
 
